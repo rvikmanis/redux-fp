c711b822ab4dec4e8eff38e35bfadb95e746a5b3
Rudolfs Vikmanis <rudolfs.vikmanis@gmail.com>
2016-09-07 23:47:30 +0300

    Add createUpdater test suite

---
+++ __test__/createUpdater.test.js
@@ -0,0 +1,56 @@
+import { createUpdater } from '../src';
+import { strictEqual } from 'assert'
+
+describe('createUpdater', () => {
+  const handleAdd = () => () => "handleAdd"
+  const handleRemove = () => () => "handleRemove"
+
+  it('returns a composition of `filterUpdater` over every key-value pair', () => {
+    const composedUpdater = createUpdater({
+      ADD: handleAdd,
+      REMOVE: handleRemove
+    })
+
+    function updater(action) {
+      if (action.type === 'ADD') {
+        return handleAdd(action)
+      }
+
+      if (action.type === 'REMOVE') {
+        return handleRemove(action)
+      }
+
+      return state => state
+    }
+
+    strictEqual(
+      composedUpdater({type: 'ADD'})("default"),
+      "handleAdd"
+    )
+
+    strictEqual(
+      updater({type: 'ADD'})("default"),
+      "handleAdd"
+    )
+
+    strictEqual(
+      composedUpdater({type: 'REMOVE'})("default"),
+      "handleRemove"
+    )
+
+    strictEqual(
+      updater({type: 'REMOVE'})("default"),
+      "handleRemove"
+    )
+
+    strictEqual(
+      composedUpdater({type: 'OTHER'})("default"),
+      "default"
+    )
+
+    strictEqual(
+      updater({type: 'OTHER'})("default"),
+      "default"
+    )
+  });
+});
